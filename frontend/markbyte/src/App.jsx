582edde8ea09da763320482b6a18d1568fd9feb7
rpangal <rpangal@purdue.edu>
2025-02-20 18:57:32 -0500

    Dyanmic routing for blog pages

--- frontend/markbyte/src/App.jsx
+++ frontend/markbyte/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Home from "./components/pages/Home/Home";
 import Login from "./components/pages/Login/Login";
 import SignUp from "./components/pages/SignUp/SignUp";
@@ -15,6 +17,25 @@ import "./App.css";
 //     </footer>
 //   );
 // }
+
+function DynamicBlogPost() {
+  const { user, post } = useParams();
+  const [htmlContent, setHtmlContent] = useState(""); // Use state for reactivity
+  useEffect(() => {
+    axios.get(`http://localhost:8080/${user}/${post}`)
+      .then((response) => {
+        setHtmlContent(response.data); // Update state
+      })
+      .catch((error) => {
+        console.error("Error fetching blogger's post page:", error);
+      });
+  }, [user, post]);
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+  );
+}
+
 function App() {
   const { isAuthenticated } = useAuth();
 
@@ -28,6 +49,7 @@ function App() {
           <div className="content">
             <Routes>
               <Route path="/" element={isAuthenticated ? <BloggerHome /> : <Home />} />
+              <Route path="/:user/:post" element={<DynamicBlogPost/>} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<SignUp />} />
             </Routes>
