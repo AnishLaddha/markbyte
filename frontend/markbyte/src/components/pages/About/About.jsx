b6f4b734e31a6294ab861677385d189606dc0a82
rpangal <rpangal@purdue.edu>
2025-02-27 18:17:11 -0500

    Some small styling changes

--- frontend/markbyte/src/components/pages/About/About.jsx
+++ frontend/markbyte/src/components/pages/About/About.jsx
@@ -1,9 +1,14 @@
-function About(){
-    return(
-        <div>
-            <!-- ANISH THROW YOUR ABOUT PAGE CONTENT IN HERE -->
-        </div>
-    )
+function About() {
+  return (
+    <div className="About relative bg-[#011A29] text-white overflow-hidden h-screen w-screen flex justify-center items-center">
+      <div className="absolute left-1/4 top-1/4 h-[500px] w-[400px] -rotate-100 rounded-[150px] bg-blue-500 opacity-15 blur-[150px] z-0"></div>
+      <div className="absolute left-[-15%] top-1/6 h-[450px] w-[350px] -rotate-15 rounded-[120px] bg-blue-500 opacity-10 blur-[130px] z-0"></div>
+      <div className="absolute right-[-10%] bottom-1/4 h-[550px] w-[450px] rotate-40 rounded-[130px] bg-blue-500 opacity-10 blur-[140px] z-0"></div>
+      {/* Anish add stuff in here */}
+
+    </div>
+  );
 }
 
 export default About;
+  
