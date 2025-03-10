ac7a1a0c3182ae46561ffb855b9044c94748e83e
Shrijan Swaminathan <shrijanswaminathan@gmail.com>
2025-03-10 15:40:36 -0400

    added md tests

--- backend/tests/api/blog_posts_test.go
+++ backend/tests/api/blog_posts_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/shrijan-swaminathan/markbyte/backend/api"
 	"github.com/shrijan-swaminathan/markbyte/backend/db"
+	"github.com/shrijan-swaminathan/markbyte/backend/auth"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
