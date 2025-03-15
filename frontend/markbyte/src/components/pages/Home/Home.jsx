ba8279ad5c81e6b8c69ecbaea9ada6cb41e06a7a
rpangal <rpangal@purdue.edu>
2025-03-15 13:37:42 -0700

    New background for home page

--- frontend/markbyte/src/components/pages/Home/Home.jsx
+++ frontend/markbyte/src/components/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { CiLogin } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
 import { useMediaQuery } from "@mui/material";
 import { FaArrowRight } from "react-icons/fa";
+import ParticlesBackground from "@/components/ui/particles";
 
 function Home() {
   const navigate = useNavigate();
@@ -11,9 +12,10 @@ function Home() {
 
   return (
     <div className="App min-h-screen flex flex-col bg-[#011A29] text-white overflow-hidden">
-      <div className="absolute left-1/4 top-1/4 h-[500px] w-[400px] -rotate-100 rounded-[150px] bg-blue-500 opacity-15 blur-[150px] z-0"></div>
+      {/* <div className="absolute left-1/4 top-1/4 h-[500px] w-[400px] -rotate-100 rounded-[150px] bg-blue-500 opacity-15 blur-[150px] z-0"></div>
       <div className="absolute left-[-15%] top-1/6 h-[450px] w-[350px] -rotate-15 rounded-[120px] bg-blue-500 opacity-10 blur-[130px] z-0"></div>
-      <div className="absolute right-[-10%] bottom-1/4 h-[550px] w-[450px] rotate-40 rounded-[130px] bg-blue-500 opacity-10 blur-[140px] z-0"></div>
+      <div className="absolute right-[-10%] bottom-1/4 h-[550px] w-[450px] rotate-40 rounded-[130px] bg-blue-500 opacity-10 blur-[140px] z-0"></div> */}
+      <ParticlesBackground id="particles" />
       <header className="header">
         <div className="logo-container" onClick={() => navigate("/")}>
           <img
