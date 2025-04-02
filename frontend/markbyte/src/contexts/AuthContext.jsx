9a4ca3ff57dc08eec88a0f6ecb16cc7e2eafe492
rpangal <rpangal@purdue.edu>
2025-04-01 20:33:48 -0400

    New auth mechanism

--- frontend/markbyte/src/contexts/AuthContext.jsx
+++ frontend/markbyte/src/contexts/AuthContext.jsx
@@ -9,39 +9,33 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      try {
-        const parsedUser = JSON.parse(storedUser);
-        const timestamp = parsedUser.time_loggedin;
-        const currentTime = new Date().getTime();
-        const timeDifference = currentTime - timestamp;
-        // if timeDifference > 72 hours, automatically set isAuthenticated to false
-        if (timeDifference > 259200000) {
-          localStorage.removeItem("user");
+    axios
+      .get(`${API_URL}/auth`, { withCredentials: true })
+      .then((response) => {
+        // check reponse code
+        if (response.status === 200) {
+          const userInfo = response.data;
+          setIsAuthenticated(true);
+          setUser({name: userInfo});
+        } else {
           setIsAuthenticated(false);
           setUser(null);
         }
-        else{
-          setIsAuthenticated(true);
-          setUser(parsedUser);
-        }
-      } catch (error) {
-        localStorage.removeItem("user");
+      })
+      .catch((error) => {
+        console.error("Error checking authentication:", error);
         setIsAuthenticated(false);
-      }
-    }
+        setUser(null);
+      });
   }, []);
 
   const login = (userInfo) => {
-    localStorage.setItem("user", JSON.stringify(userInfo));
     setIsAuthenticated(true);
     setUser(userInfo);
   };
 
   const logout = () => {
     axios.post(`${API_URL}/logout`, {}, { withCredentials: true });
-    localStorage.removeItem("user");
     setIsAuthenticated(false);
     setUser(null);
   };
